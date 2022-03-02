af68ae608b3951ef31ca72d65ce5e61b5fa209ed
blagojevic-nj <blagojevic.njegos1999@gmail.com>
2022-03-02 11:41:20 +0100

    Password reset implemented completely

--- insta-clone-front/src/router/MyRoutes.js
+++ insta-clone-front/src/router/MyRoutes.js
@@ -2,16 +2,19 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import ForgottenPassword from "../components/ForgottenPassword/ForgottenPassword";
 import Home from "../components/Home";
 import Login from "../components/Login/Login";
-import NotFound from "../components/NotFoundPage";
+import NotFound from "../components/NotFoundPage/NotFoundPage";
 import Registration from "../components/Registration/Registration";
+import ResetPassword from "../components/ResetPassword/ResetPassword";
 import * as helper from "../helpers/AuthHelper"
 
 const MyRoutes = () => {
     return <Routes>
+        <Route exact path="" element={helper.isAuthenticated() ? <Home/> : <Navigate to={"/login"}/>}/>
         <Route exact path="/home" element={helper.isAuthenticated() ? <Home/> : <Navigate to={"/login"}/>}/>
         <Route exact path="/login" element={!helper.isAuthenticated() ? <Login/> :<Navigate to={"/home"}/>}/>
         <Route exact path="/registration" element={!helper.isAuthenticated() ? <Registration/> :<Navigate to={"/home"}/>}/>
         <Route exact path="/forgotten-password" element={helper.isAuthenticated() ? <Home/> : <ForgottenPassword/>}/>
+        <Route exact path="/reset-password/:token" element={helper.isAuthenticated() ? <Home/> : <ResetPassword/>}/>
         <Route exact path="*" element={<NotFound/>}/>
     </Routes>
 }
