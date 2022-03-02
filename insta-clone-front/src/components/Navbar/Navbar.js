3d6c7db0c40686bbecd69914e75eab9da2677008
blagojevic-nj <blagojevic.njegos1999@gmail.com>
2022-03-02 15:00:13 +0100

    a.href updated

--- insta-clone-front/src/components/Navbar/Navbar.js
+++ insta-clone-front/src/components/Navbar/Navbar.js
@@ -16,14 +16,14 @@ const Navbar = () => {
 
     return <>
         <nav className="my-navbar navbar sticky-top navbar-light justify-content-around">
-            <a className="navbar-brand ml-2" href="#">
+            <a className="navbar-brand ml-2" href="http://localhost:3000/home">
             <img alt="Instagram" src={logo} height="30px"/>
             </a>
             <form  className="form-inline my-search" onSubmit={(e)=>search(e)}>
                 <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" onChange={(e) => setQuery(e.target.value)}/>
             </form>
             <div className="options d-flex justify-content-center">
-                <a href="#" tabindex="0">
+                <a href="http://localhost:3000/home" tabindex="0">
                     <svg className="_8-yf5 " color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24">
                     <path d="M22 23h-6.001a1 1 0 01-1-1v-5.455a2.997 2.997 0 10-5.993 0V22a1 1 0 01-1 1H2a1 1 0 01-1-1V11.543a1.002 1.002 0 01.31-.724l10-9.543a1.001 1.001 0 011.38 0l10 9.543a1.002 1.002 0 01.31.724V22a1 1 0 01-1 1z"></path>
                     </svg>
