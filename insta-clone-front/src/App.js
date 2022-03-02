433d4741fdb5c1545e0c898c91afe772042abe22
blagojevic-nj <blagojevic.njegos1999@gmail.com>
2022-03-02 14:56:44 +0100

    Navbar removed from home component

--- insta-clone-front/src/App.js
+++ insta-clone-front/src/App.js
@@ -4,9 +4,14 @@ import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"
 import MyRouter from "./router/MyRouter";
 import "./helpers/interceptors/TokenInterceptor"
 
+import * as authHelper from "./helpers/AuthHelper"
+import Navbar from "./components/Navbar/Navbar";
+
 function App() {
-  return (
+  return (<>
+    {authHelper.isAuthenticated() && <Navbar/>}
     <MyRouter/>
+  </>
   );
 }
 
--- insta-clone-front/src/components/Home/Home.js
+++ insta-clone-front/src/components/Home/Home.js
@@ -1,10 +1,8 @@
 import Feed from "../Feed/Feed";
-import Navbar from "../Navbar/Navbar";
 
 const Home = () => {
     return (
         <div className="home">
-            <Navbar/>
             <Feed></Feed>
         </div>  
     )
