b9ce26bc07ae538ad235f36105c9efc8f8d8332b
blagojevic-nj <blagojevic.njegos1999@gmail.com>
2022-03-03 16:52:25 +0100

    reactions count update

--- insta-clone-front/src/components/Post/Reactions/ReactionsBar.js
+++ insta-clone-front/src/components/Post/Reactions/ReactionsBar.js
@@ -2,11 +2,15 @@ import React from "react";
 import { Reactions } from "./Reactions/Reactions";
 import "./ReactionsBar.css";
 
-const ReactionsBar = ({ entityId }) => {
-  const likes = 2209;
+const ReactionsBar = ({ entityId, likes, setLikes }) => {
   return (
     <div className="reactions-container">
-      <Reactions reactionKind={"POST_REACTION"} entityId={entityId} />
+      <Reactions
+        reactionKind={"POST_REACTION"}
+        entityId={entityId}
+        likes={likes}
+        setLikes={setLikes}
+      />
       <div className="likes">
         <p className="likes-text">Liked By {likes} people</p>
       </div>
