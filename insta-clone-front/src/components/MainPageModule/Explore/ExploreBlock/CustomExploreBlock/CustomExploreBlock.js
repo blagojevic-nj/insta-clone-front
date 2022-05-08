57b80f598c560457d921836a8da898f8236393d7
Luka Kureljušić <lkureljusic@ymail.com>
2022-05-08 21:22:49 +0200

    small bugfix

--- insta-clone-front/src/components/MainPageModule/Explore/ExploreBlock/CustomExploreBlock/CustomExploreBlock.js
+++ insta-clone-front/src/components/MainPageModule/Explore/ExploreBlock/CustomExploreBlock/CustomExploreBlock.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import "./CustomExploreBlock.css"
+import { REACT_APP_URL } from '../../../../../helpers/constants'
+
 
 const CustomExploreBlock = ({ posts }) => {
+
+  const viewPost = (id) => {
+    window.location.href = `${REACT_APP_URL}/post/${id}`
+  }
   return (
 
     posts && posts[0] ?
@@ -9,29 +15,36 @@ const CustomExploreBlock = ({ posts }) => {
       <div className='custom-explore-block'>
         <div className='custom-explore-small-col'>
           <div className='custom-post-container-sm'>
-                  <img className="custom-post"
-                    alt="loading..."
-                    src={process.env.REACT_APP_SERVER_URL + posts[0].picture}
-                  ></img>
+            <img className="custom-post"
+              alt="loading..."
+              src={process.env.REACT_APP_SERVER_URL + posts[0].picture}
+              onClick={() => viewPost(posts[0].id)}
+            ></img>
           </div>
 
-          <div className='custom-post-container-sm'>
-                  <img className="custom-post"
-                    alt="loading..."
-                    src={process.env.REACT_APP_SERVER_URL + posts[0].picture}
-                  ></img>
-          </div>
+          {!!posts[2] ?
+            <div className='custom-post-container-sm'>
+              <img className="custom-post"
+                alt="loading..."
+                src={process.env.REACT_APP_SERVER_URL + posts[2].picture}
+                onClick={() => viewPost(posts[2].id)}
+              ></img>
+            </div> 
+            : <></>}
         </div>
-        <div className='custom-explore-big-col'>
-          <div className='custom-post-container-lg'>
-                  <img className="custom-post"
-                    alt="loading..."
-                    src={process.env.REACT_APP_SERVER_URL + posts[0].picture}
-                  ></img>
-          </div>
-        </div>
+          {!!posts[1] ?
+            <div className='custom-explore-big-col'>
+              <div className='custom-post-container-lg'>
+                <img className="custom-post"
+                  alt="loading..."
+                  src={process.env.REACT_APP_SERVER_URL + posts[1].picture}
+                  onClick={() => viewPost(posts[1].id)}
+                ></img>
+              </div>
+            </div>
+            : <></>}
+
       </div>
-
       : <></>
   )
 }
--- insta-clone-front/src/components/MainPageModule/Explore/ExploreBlock/RegularExploreBlock/RegularExploreBlock.js
+++ insta-clone-front/src/components/MainPageModule/Explore/ExploreBlock/RegularExploreBlock/RegularExploreBlock.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import "./RegularExploreBlock.css"
+import { REACT_APP_URL } from '../../../../../helpers/constants'
 
 const RegularExploreBlock = ({ posts }) => {
+
+
+    const viewPost = (id) => {
+        window.location.href = `${REACT_APP_URL}/post/${id}`
+    }
+
     return (
         posts && posts[0] ?
 
@@ -11,6 +18,7 @@ const RegularExploreBlock = ({ posts }) => {
                         <img className="explore-post"
                             alt="loading..."
                             src={process.env.REACT_APP_SERVER_URL + post.picture}
+                            onClick={() => viewPost(post.id)}
                         ></img>
                     </div>
                 )}
